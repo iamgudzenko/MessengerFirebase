975cfc9cabee16c95a0215244509c88d1b1cbaa9
agydz <gudzenko.alo@gmail.com>
2022-04-30 23:36:43 +0300

    add ToolBar and DrawerLayout

--- app/src/main/java/com/example/testmes/ChatsActivity.kt
+++ app/src/main/java/com/example/testmes/ChatsActivity.kt
@@ -1,11 +1,22 @@
 package com.example.testmes
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.view.View.OnTouchListener
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat.setWindowInsetsAnimationCallback
+import androidx.core.widget.addTextChangedListener
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testmes.Model.Chats
 import com.example.testmes.Model.Users
@@ -13,10 +24,9 @@ import com.example.testmes.Presenter.*
 import com.example.testmes.View.ChatsView
 import com.example.testmes.View.ISearchUserView
 import com.example.testmes.databinding.ActivityChatsBinding
-import com.example.testmes.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+
 
 class ChatsActivity : AppCompatActivity(), ChatsView, ISearchUserView {
     lateinit var auth: FirebaseAuth
@@ -24,13 +34,21 @@ class ChatsActivity : AppCompatActivity(), ChatsView, ISearchUserView {
     private lateinit var adapter: ChatsAdapter
     lateinit var chatsPresenter: IChatsPresenter
     lateinit var searchUser: ISearchUser
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawLayout: DrawerLayout
+    lateinit var navView: NavigationView
+    lateinit var toolbar: Toolbar
 
     var chatsUser: MutableList<Chats?> = mutableListOf()
 
+    @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getSupportActionBar()?.hide();
+        nawigation()
 
         chatsPresenter = ChatsPresenter(this, this)
         chatsPresenter.loadingChats()
@@ -55,13 +73,58 @@ class ChatsActivity : AppCompatActivity(), ChatsView, ISearchUserView {
         binding.recyclerViewChats.layoutManager = layoutManager
         binding.recyclerViewChats.adapter = adapter
 
+        binding.editSearchPhoneNumberUser.setOnTouchListener(OnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_MOVE) {
+                v.isFocusableInTouchMode = true
+                binding.editSearchPhoneNumberUser.clearFocus()
+            }
+            v.performClick()
+        })
+
         binding.butSearchUser.setOnClickListener {
             searchUser.userSearch(binding.editSearchPhoneNumberUser.text.toString())
         }
 
+        binding.recyclerViewChats.setOnClickListener {
+            binding.editSearchPhoneNumberUser.clearFocus()
+        }
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onBackPressed() {
+        binding.editSearchPhoneNumberUser.clearFocus()
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+    }
+
+    fun nawigation(){
+        drawLayout = findViewById(R.id.drawLayout)
+        toolbar = findViewById(R.id.toolbar)
+        toggle = ActionBarDrawerToggle(this, drawLayout, toolbar, R.string.open, R.string.close)
+        navView = findViewById(R.id.navView)
+
+        drawLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.item1 -> Toast.makeText(applicationContext,
+                    "Item 1", Toast.LENGTH_SHORT).show()
+                R.id.item2 -> Toast.makeText(applicationContext,
+                    "Item 2", Toast.LENGTH_SHORT).show()
+                R.id.item3 -> Toast.makeText(applicationContext,
+                    "Item 3", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
     @SuppressLint("NotifyDataSetChanged")
     override fun listChatsUser(chat: Chats?) {
         Log.w("CHAT ID", chat?.loginUserChatWith.toString())
